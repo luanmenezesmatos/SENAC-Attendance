9fdc579a0f5eed49e38e7b0363720463f21f8bd2
Luan Menezes <luanmenezesmluiz@gmail.com>
2023-10-25 17:06:05 -0300

    chore: commitando as últimas alterações

--- src/app/environmentApp.py
+++ src/app/environmentApp.py
@@ -75,6 +75,7 @@ class environmentApp:
 
                         add_values = ni.add_values(
                             values={
+                                "Status": [entry['Status']],
                                 "Componente Curricular": [entry['Componente Curricular']],
                                 "Descrição": [entry['Descrição']],
                                 "Notas/Menções": [entry['Notas/Menções']],
